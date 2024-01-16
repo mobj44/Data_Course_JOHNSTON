228b33af7c3fcf34f9a2277804a961b38c16d333
Morgan Johnston <morganbjohnston@gmail.com>
2024-01-16 11:09:42 -0700

    assignment 2 complete

---
+++ Notes/listing_files.R
@@ -0,0 +1,18 @@
+# 4
+csv_files <- list.files(recursive=TRUE, pattern='.csv')
+csv_files
+
+# 5
+length(csv_files)
+
+# 6
+df <- read.csv('Data/wingspan_vs_mass.csv', header = TRUE)
+
+# 7
+
+
+# 8
+
+# 9
+
+# 10
