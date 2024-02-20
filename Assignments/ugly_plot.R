3b3eb6357163b62c16b4c6586bfa4f6db6640ebe
Morgan Johnston <morganbjohnston@gmail.com>
2024-02-20 09:01:47 -0700

    ugly plot

--- Assignments/ugly_plot.R
+++ Assignments/ugly_plot.R
@@ -3,18 +3,11 @@ library(palmerpenguins)
 library(ggimage)
 library(GGally)
 
-
-ggpairs(penguins)
 str(penguins)
 
-plot1 <- penguins %>%
-    ggplot() +
-    geom_density2d_filled(aes(x = bill_depth_mm, y = bill_length_mm, color = island, fill = species)) +
-    geom_image(aes(x = bill_depth_mm, y = bill_length_mm, image= image_path), size = 0.09) +
-    theme(panel.background = element_rect(fill = 'limegreen'),
-          panel.grid = element_line(color = 'yellow', linewidth = 3),
-          plot.background = element_rect('red'))
-plot1
+penguins$image_path <- penguins$species %>%
+    as.character() %>%
+    paste0('./penguin_images/', ., '.png')
 
 penguin_plot <- penguins %>%
     ggplot(aes(x = bill_depth_mm, y = bill_length_mm)) +
@@ -30,18 +23,10 @@ penguin_plot <- penguins %>%
           plot.title = element_text(hjust = .5, color = 'purple')) +
     scale_y_continuous(breaks = c(40,50,60), labels = c('smol', 'okrrr', 'big chongus')) +
     scale_x_continuous(breaks = c(15, 17.5, 20.0), labels = c('smol', 'okrrr', 'big chongus')) +
-    labs(title = "Face size", x = "length", y = "depth")
+    labs(title = "Face size", x = "Nibble Knob", y = "Bitey Bit")
 
 ggsave('./penguin_plot.png', dpi = 80)
 
-penguins$image_path <- penguins$species %>%
-    as.character() %>%
-    paste0('./penguin_images/', ., '.png')
 
-
-penguins %>%
-    ggplot(aes(x = bill_depth_mm, y = bill_length_mm)) +
-    geom_density_2d_filled() +
-    geom_image(aes(image= image_path), size = 0.05)
 
 
