5200c183d81a2a6f0ecf870bc68d458f600adafb
Morgan Johnston <morganbjohnston@gmail.com>
2024-02-20 15:12:59 -0700

    Notes

--- Notes/another_tidy.R
+++ Notes/another_tidy.R
@@ -14,7 +14,7 @@ n_visits <- dat %>%
 
 names(bp)[which(grepl("^BP", names(bp)))] <- paste0('visit',1:n_visits)
 
-bp %>%
+bp <- bp %>%
     pivot_longer(starts_with("visit"),
                  names_to = "visit",
                  values_to = "bp",
@@ -32,7 +32,7 @@ n_visits <- dat %>%
 
 names(hr)[which(grepl("^HR", names(hr)))] <- paste0('visit',1:n_visits)
 
-hr %>%
+hr <- hr %>%
     pivot_longer(starts_with("visit"),
                  names_to = "visit",
                  values_to = "hr",
@@ -42,4 +42,21 @@ hr %>%
 bp <- clean_names(bp)
 hr <- clean_names(hr)
 
+df <- full_join(bp, hr)
+
+df <- df %>%
+    separate(bp, into = c('systolic', 'diastolic')) %>%
+    mutate(systolic_bp = systolic %>% as.numeric(),
+           diastolic_bp = diastolic %>% as.numeric()) %>%
+    pivot_longer(cols = c("systolic", "diastolic"),
+                 names_to = "bp_type",
+                 values_to = "bp") %>%
+    mutate(race = case_when(
+        race == "WHITE" | race == "Caucasian" ~ "White",
+        TRUE ~ race)) %>%
+    mutate(hispanic = case_when(hispanic == "Hispanic" ~ TRUE,
+                                TRUE ~ FALSE)) %>%
+    mutate(birthdate = paste(year_birth,month_of_birth,day_birth,sep = "-") %>%
+               as.POSIXct())  %>% # posixct universal time code down to ns, knows calendar
+    select(-pat_id,-month_of_birth,-day_birth,-year_birth)
 
